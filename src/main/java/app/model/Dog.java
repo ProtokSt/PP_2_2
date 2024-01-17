54596256f6b9d20c4fcde43c8baea5d6618ddabf
Ratmir Protok <gerkon9@gmail.com>
2024-01-18 00:23:19 +0300

    Qualifier in constructor

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -2,10 +2,10 @@ package app.model;
 
 import org.springframework.stereotype.Component;
 
-@Component("catAnimal")
-public class Cat extends Animal {
+@Component("dogAnimal")
+public class Dog extends Animal {
     @Override
     public String toString() {
-        return "Im a Cat";
+        return "Im a Dog";
     }
 }
